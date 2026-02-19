9e2568121eb9937b58deafcf1c6f3084af6b4586
kcbyk <141924954+kcbyk@users.noreply.github.com>
2026-02-19 05:43:08 +0300

    Noxify Omega v29 Commit: tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
